dd7f7be500a37a2cd63715d6c4dcaf39b44e1b54
Harsh Kumar <kumarharsh06.bds@gmail.com>
2026-03-15 01:24:06 +0530

    Add static files and file response imports

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from contextlib import asynccontextmanager
 import hashlib
 import json
