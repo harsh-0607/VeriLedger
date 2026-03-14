d3b22c7e929db8eb5dc90db793c438779a10f28f
Harsh Kumar <kumarharsh06.bds@gmail.com>
2026-03-14 23:17:14 +0530

    Add storage methods for blockchain persistence
    Implement methods to save and load blockchain data to and from a JSON file.

--- app/storage.py
+++ app/storage.py
@@ -1,1 +1,34 @@
+import json
+import os
+from .blockchain import Blockchain, Block
 
+CHAIN_FILE = "ledger.json"
+
+class Storage:
+    @staticmethod
+    def save_chain(blockchain: Blockchain):
+        with open(CHAIN_FILE, 'w') as f:
+            json.dump([block.to_dict() for block in blockchain.chain], f, indent=4)
+
+    @staticmethod
+    def load_chain(blockchain: Blockchain):
+        if not os.path.exists(CHAIN_FILE):
+            Storage.save_chain(blockchain)
+            return
+
+        with open(CHAIN_FILE, 'r') as f:
+            try:
+                data = json.load(f)
+                blockchain.chain = []
+                for b_dict in data:
+                    block = Block(
+                        index=b_dict["index"],
+                        timestamp=b_dict["timestamp"],
+                        data=b_dict["data"],
+                        previous_hash=b_dict["previous_hash"],
+                        nonce=b_dict["nonce"]
+                    )
+                    block.hash = b_dict["hash"]
+                    blockchain.chain.append(block)
+            except json.JSONDecodeError:
+                Storage.save_chain(blockchain)
